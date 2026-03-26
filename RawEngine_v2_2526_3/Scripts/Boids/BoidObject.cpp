aaf495a22d8651067602f6cee21fc93ac03a855f
DeGekkeLamas <karsvdtoorn@gmail.com>
2026-03-26 15:37:03 +0100

    boids start

--- RawEngine_v2_2526_3/Scripts/Boids/BoidObject.cpp
+++ RawEngine_v2_2526_3/Scripts/Boids/BoidObject.cpp
@@ -1,5 +1,36 @@
-//
-// Created by karsv on 16-3-2026.
-//
-
 #include "BoidObject.hpp"
+
+void BoidObject::Awake() {
+    boids.push_back(this);
+}
+
+
+void BoidObject::Update(float deltaTime) {
+    glm::vec3 perceivedCenter = glm::vec3();
+    for (int i = 0; i < boids.size(); i++) {
+        BoidObject* currentBoid = boids[i];
+        if (currentBoid == this) continue;
+
+        // Center of mass
+        perceivedCenter += currentBoid->transform.position();
+    }
+    perceivedCenter /= boids.size()-1;
+    perceivedCenter -= this->transform.position();
+
+    velocity += perceivedCenter;
+    transform.TranslateWorldSpace(velocity);
+}
+
+glm::vec3 BoidObject::FlyToCenterOfMass(BoidObject* compare) {
+    glm::vec3 perceivedCenter;
+}
+
+glm::vec3 BoidObject::KeepDistance(BoidObject* compare) {
+
+}
+
+glm::vec3 BoidObject::MatchVelocity(BoidObject* compare) {
+
+}
+
+
--- RawEngine_v2_2526_3/Scripts/Boids/BoidObject.hpp
+++ RawEngine_v2_2526_3/Scripts/Boids/BoidObject.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include "Scripts/Engine/RenderableObject.hpp"
 
-class BoidObject {
+class BoidObject : public RenderableObject {
+public:
+        glm::vec3 velocity;
+        std::vector<BoidObject*> boids;
+        void Awake();
+        void Update(float deltaTime);
+private:
+        glm::vec3 FlyToCenterOfMass(BoidObject* compare);
+        glm::vec3 KeepDistance(BoidObject* compare);
+        glm::vec3 MatchVelocity(BoidObject* compare);
 };
 
 
